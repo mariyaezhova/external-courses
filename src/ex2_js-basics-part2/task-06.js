6ce23035ea71982fb29f0e5792c867a1a21fe351
Mariya Ezhova <mariyaezhova88@gmail.com>
2020-09-26 21:37:37 +0300

    Ex2_js-basics-part2: Do task 6
    Write a function reсeives any number more than 1000 and returns prime number or not.
    If number is more than 1000 the massage is displayed "the data is incorrect".
    Pay attantion on 0 and 1.
    Examples of function's work:
    f(13);
    // Число 13 - простое число
    
    f(14);
    // Число 14 - составное число

---
+++ src/ex2_js-basics-part2/task-06.js
@@ -0,0 +1,17 @@
+function isPrime(number) {
+    if ((number <= 1) || (number > 1000)) {
+        return 'Данные неверны';
+    }
+    for (let i = 2; i ** 2 <= number; i++ ) {
+        if (number % i === 0) {
+            return `Число ${number} - составное число`;
+        }
+    }
+    return `Число ${number} - простое число`;
+}
+isPrime(15); // Число 88 - составное число
+isPrime(631); // Число 631 - простое число
+isPrime(2); // Число 2 - простое число
+isPrime(-5); // Данные неверны
+isPrime(1010); // Данные неверны
+module.exports = isPrime;
