5f1df795cb3a3f441ee8b7bc807b6e4fb70c2dd4
Mariya Ezhova <mariyaezhova88@gmail.com>
2020-11-14 21:17:40 +0300

    Kanban: Dropdown code refactoring

--- src/kanban/task-03.js
+++ src/kanban/task-03.js
@@ -3,25 +3,22 @@ const arrow = document.querySelector('.arrow-down');
 const main = document.querySelector('.main');
 const menu = document.createElement('ul');
 
-const liElement1 = document.createElement('li');
-const liElement2 = document.createElement('li');
-const liElement3 = document.createElement('li');
-const liElement4 = document.createElement('li');
-const liElement5 = document.createElement('li');
+const menuTextItems = ['Your profile', 'Your tasks', 'Help', 'Settings', 'Sign out'];
 
-const hrElement1 = document.createElement('hr');
-const hrElement2 = document.createElement('hr');
+menuTextItems.forEach((element, index) => {
+    const liElement = document.createElement('li');
+    liElement.classList.add('menu-item');
+    liElement.innerText = element;
+    menu.appendChild(liElement);
+    
+    if (index % 2 !== 0) {
+        hrElement = document.createElement('hr');
+        hrElement.classList.add('menu-stroke');
+        menu.appendChild(hrElement);
+    }
+});
 
-liElement1.innerText = 'Your profile';
-liElement2.innerText = 'Your tasks';
-liElement3.innerText = 'Help';
-liElement4.innerText = 'Settings';
-liElement5.innerText = 'Sign out';
-
-[liElement1, liElement2, liElement3, liElement4, liElement5].forEach((element) => element.classList.add('menu-item'));
-[hrElement1, hrElement2].forEach(element => element.classList.add('menu-stroke'));
 menu.classList.add('pull-down-menu');
-[liElement1, liElement2, hrElement1, liElement3, liElement4, hrElement2, liElement5].forEach((element) => menu.appendChild(element));
 
 function showMenu() {
     main.append(menu);
